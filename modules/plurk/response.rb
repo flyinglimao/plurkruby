c8091359b98bf7c574c895b9b1a9f730b46d6a08
Bill Wu <hi@limaois.me>
2020-03-05 23:21:05 -0500

    Feat: Add response module

---
+++ modules/plurk/response.rb
@@ -0,0 +1,24 @@
+module Plurk
+  module Response
+    def add_response(plurk_id = nil, content = nil, qualifier = ':', **hash)
+      hash["plurk_id"] ||= plurk_id
+      hash["content"] ||= content
+      hash["qualifier"] ||= qualifier
+
+      access "/APP/Responses/responseAdd", hash
+    end
+
+    def get_response(plurk_id = nil, **hash)
+      hash["plurk_id"] ||= plurk_id
+
+      access "/APP/Responses/get", hash
+    end
+
+    def delete_response(plurk_id = nil, response_id = nil, **hash)
+      hash["response_id"] ||= response_id
+      hash["plurk_id"] ||= plurk_id
+
+      access "/APP/Responses/responseDelete", hash
+    end
+  end
+end
--- modules/plurk_client.rb
+++ modules/plurk_client.rb
@@ -1,7 +1,9 @@
-require_relative "../modules/plurk_module.rb"
+require_relative "plurk_module.rb"
 require_relative "plurk/timeline.rb"
+require_relative "plurk/response.rb"
 
 class PlurkClient
   extend PlurkModule
   include Plurk::Timeline
+  include Plurk::Response
 end
